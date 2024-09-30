7c3f3b1d2f73b4a39ba0e74d89a001e8270d15fa
Smart-imran <rst.imran4@gmail.com>
2024-10-01 00:06:23 +0600

    add visited

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 
 const Country = ({ countryparam }) => {
 
     console.log(countryparam)
 
     const { name, flags, population, area, cca3 } = countryparam;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () =>{
+        setVisited(!visited)
+    }
 
     return (
         <div className="border-4 border-indigo-600 pl-8 rounded-md">
@@ -12,7 +19,13 @@ const Country = ({ countryparam }) => {
             <p >Population: {population}</p>
             <p>Area: {area}</p>
             <p className="font-bold">Code: {cca3}</p>
-            <button>visited</button>
+            <button onClick={handleVisited} className="border-2 p-2 bg-indigo-500 text-white border-violet-600 rounded-md">visited</button>
+
+                {
+                    visited ? 'I have visited' : 'I want to visited'
+                }
+
+
         </div>
     );
 };

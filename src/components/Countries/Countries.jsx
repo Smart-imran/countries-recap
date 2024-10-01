339db7e7b4c76a21b2434a83ecfc0cf929898f9e
Smart-imran <rst.imran4@gmail.com>
2024-10-01 10:48:46 +0600

    add

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -6,28 +6,47 @@ const Countries = () => {
 
     const [countries, setCountries] = useState([]);
 
-    useEffect(()=>{
+    const[visitedCountries, setVisitedcountries] = useState([]);
+
+    const handlevisitedCountries = (countryparm1) =>{
+        console.log('add this country');
+        console.log(countryparm1);
+    }
+
+    useEffect(() => {
 
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data =>setCountries(data))
+            .then(res => res.json())
+            .then(data => setCountries(data))
 
-    },[])
+    }, [])
 
     return (
         <div >
-             <h3 className="p-10 text-5xl">Countries: {countries.length} </h3>
-           <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-           {
-                countries.map(country => <Country
-                
-                countryparam={country} key={country.cca3}
-                
-                
-                ></Country>)
-            }
-           </div>
-           
+            <h3 className="p-10 text-5xl">Countries: {countries.length} </h3>
+
+            <div>
+                <h5>Visited Countries : </h5>
+                <ul>
+
+                </ul>
+            </div>
+
+            <br />
+
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {
+                    countries.map(country => <Country
+
+                        countryparam={country} key={country.cca3}
+
+                        handlevisitedCountries = {handlevisitedCountries}
+
+
+                    ></Country>)
+                }
+            </div>
+
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Country = ({ countryparam }) => {
+const Country = ({ countryparam , handlevisitedCountries }) => {
 
     console.log(countryparam)
 
@@ -12,19 +12,25 @@ const Country = ({ countryparam }) => {
         setVisited(!visited)
     }
 
+    console.log(handlevisitedCountries);
+
     return (
-        <div className="border-4 border-indigo-600 pl-8 rounded-md">
+        <div className={`border-4 border-indigo-600 pl-8 rounded-md ${visited ? 'bg-red-600' : ''}`}>
+
             <h3 className='p-1'>Name: {name.common}</h3>
             <img src={flags?.png} alt="" />
             <p >Population: {population}</p>
             <p>Area: {area}</p>
             <p className="font-bold">Code: {cca3}</p>
-            <button onClick={handleVisited} className="border-2 p-2 bg-indigo-500 text-white border-violet-600 rounded-md">visited</button>
+
+
+            <button className="border-2 p-2 bg-indigo-500 text-white border-violet-600 rounded-md mr-6 ">Mark Visited</button><br />
+
+            <button onClick={handleVisited} className="border-2 p-2 bg-indigo-500 text-white border-violet-600 rounded-md mt-4">{visited ? 'visited':'Going'}</button>
 
                 {
                     visited ? 'I have visited' : 'I want to visited'
                 }
-
 
         </div>
     );
